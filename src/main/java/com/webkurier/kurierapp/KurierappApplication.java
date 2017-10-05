8bd29f60b46726b5442dd021b529f77d47e5a814
Unknown <michall.romann@gmail.com>
2017-10-05 12:17:56 +0200

    Dodanie czystego projektu.

---
+++ src/main/java/com/webkurier/kurierapp/KurierappApplication.java
@@ -0,0 +1,12 @@
+package com.webkurier.kurierapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurierappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurierappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/webkurier/kurierapp/KurierappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webkurier.kurierapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KurierappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
